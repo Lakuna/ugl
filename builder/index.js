373c3ef91cf85c0b68b02c1c2560115c27e8f421
T3rrabyte <tjmartin2003@gmail.com>
2020-10-22 06:59:48 -0500

    WIP

--- builder/index.js
+++ builder/index.js
@@ -5,15 +5,20 @@ const fs = require('fs');
 const columnify = require('columnify');
 
 const API = 'https://lakuna.pw/api/umbra.json';
+const REQUIRED_TAG_NAME = 'REQUIRED';
+const LOCAL_VERSION_NAME = 'local';
+const LOCAL_FILE_NAME = 'umbra.js';
 
+// Get versions from API.
+console.log('Checking API for versions...');
 request(API, (err, res, body) => {
 	if (err) { 
-		console.error(`Error accessing Umbra API: ${err}.\nUsing offline mode.`);
-
 		// Offline mode.
-		return chooseVersion({ latest: 'local', recommended: 'local', releases: [] });
+		console.error(`Error accessing Umbra API. Using offline mode.\n${err}`);
+		return chooseVersion({ latest: LOCAL_VERSION_NAME, recommended: LOCAL_VERSION_NAME, releases: [] });
 	}
 
+	// Online mode.
 	return chooseVersion(JSON.parse(body));
 });
 
@@ -30,7 +35,7 @@ const chooseVersion = (versions) => {
 	});
 
 	// Add local option to build a distribution from a downloaded umbra.js file.
-	localVersion = { id: 'local', url: 'umbra.js' };
+	localVersion = { id: LOCAL_VERSION_NAME, url: LOCAL_FILE_NAME };
 	versionMap.push(localVersion);
 	info += localVersion.id;
 
@@ -43,7 +48,7 @@ const chooseVersion = (versions) => {
 		// Close the input source.
 		rl.close();
 
-		if (input.trim() == '') { input = versions.recommended}
+		if (input.trim() == '') { input = versions.recommended; }
 
 		// Get version information.
 		const version = versionMap.find((version) => version.id == input);
@@ -52,14 +57,14 @@ const chooseVersion = (versions) => {
 		if (version.id == localVersion.id) {
 			// Use local version if selected.
 			fs.readFile(localVersion.url, 'utf8', (err, data) => {
-				if (err) { return console.error(`Error reading ${localVersion.url}: ${err}`); }
+				if (err) { return console.error(`Error reading ${localVersion.url}.\n${err}`); }
 
 				return parseVersion(data);
 			});
 		} else {
 			// Use an online version.
 			request(version.url, (err, res, body) => {
-				if (err) { return console.error(err); }
+				if (err) { return console.error(`Error requesting ${version.url}.\n${err}`); }
 
 				return parseVersion(body);
 			});
@@ -67,7 +72,7 @@ const chooseVersion = (versions) => {
 	});
 }
 
-const parseVersion = (script) => {
+const parseVersion = async (script) => {
 	class Line {
 		constructor() {
 			this.tags = [];
@@ -94,7 +99,7 @@ const parseVersion = (script) => {
 	const inMultilineTags = [];
 
 	// Build Tag and Line objects.
-	script.split(/[\r\n]+/).forEach((line) => {
+	for (let line of script.split(/[\r\n]+/)) {
 		// Line details.
 		let inComment = false;
 		const inTags = [];
@@ -125,64 +130,72 @@ const parseVersion = (script) => {
 
 		// Check line for tag definitions.
 		const comment = parsedLine.comment.split(/ +/);
-		if (comment[0] == 'UTAGDEF') {
+		const operator = comment[0];
+		const tagName = comment[2];
+		const propertyName = comment[1];
+		const data = comment.slice(3).join(' ').trim();
+		if (operator == 'UTAGDEF') {
 			// Line defines a property of a tag.
 
 			// Find or create the tag.
-			let tag = tags.find((tag) => tag.name == comment[2]);
-			if (!tag) { tag = new Tag(comment[2]); tags.push(tag); }
-
-			// Get the data in the definition.
-			const data = comment.slice(3).join(' ').trim();
+			let tag = tags.find((tag) => tag.name == tagName);
+			if (!tag) { tag = new Tag(tagName); tags.push(tag); }
 
 			// Assign the data to the designated property.
-			if (comment[1] == 'DESC') {
+			if (propertyName == 'DESC') {
 				// Assign tag description.
 				tag.description = data;
-			} else if (comment[1] == 'REQU') {
+			} else if (propertyName == 'REQU') {
 				// Add a tag requirement.
 				const requiredTag = tags.find((tag) => tag.name == data);
-				if (!requiredTag) { return console.error(`Unable to require tag "${data}".`); }
+				if (!requiredTag) { return console.error(`Unable to require unknown tag "${data}".`); }
 
 				tag.requiresTags.push(requiredTag);
-			} else if (comment[1] == 'LINK') {
+			} else if (propertyName == 'SIZE') {
+				console.log(`Ignoring outdated tag "${propertyName}".`);
+			} else if (propertyName == 'LINK') {
+				// TODO: Figure out why this is still acting async.
+
 				// Include code from URL.
-				request(data, (err, res, body) => {
-					if (err) { return console.error(`Error getting link ${data} for tag "${tag.name}".`); }
+				console.log('Making request.'); // TODO: Delete.
+				await request(data, (err, res, body) => {
+					if (err) { return console.error(`Error getting link ${data} for tag "${tag.name}".\n${err}`); }
 
 					// Uglify linked code.
 					let linkedCode = body;
 					const uglified = uglifyjs.minify(linkedCode);
-					if (uglified.error) { return console.error(`Error uglifying code: ${uglified.error}.\nLinking expanded code.`); } else { linkedCode = uglified.code; }
+					if (uglified.error) { console.error(`Error uglifying code. Linking expanded code.\n${uglified.error}`); } else { linkedCode = uglified.code; }
 
 					// Add linked code to file as lines.
 					const linkLine = new Line();
-					linkLine.comment = `Linked from ${body}.`;
+					linkLine.comment = `Linked from ${data}.`;
 					linkLine.code = linkedCode;
 					linkLine.tags.push(tag);
 					lines.push(linkLine);
+					console.log('Request complete.'); // TODO: Delete.
 				});
-			} else { return console.error(`Unknown tag property "${comment[1]}".`); }
+				console.log('Made request.'); // TODO: Delete.
+			} else { return console.error(`Unknown tag property "${propertyName}".`); }
 		}
 
 		// Check line for tag designations.
-		else if (comment[0] == 'UTAGSET') {
+		else if (operator == 'UTAGSET') {
 			// Find the tag.
-			let tag = tags.find((tag) => tag.name == comment[2]);
-			if (!tag) { return console.error(`Unknown tag "${comment[2]}".`); }
+			let tag = tags.find((tag) => tag.name == tagName);
+			if (!tag) { return console.error(`Unknown tag "${tagName}".`); }
 
 			// Assign tag to lines based on designation.
-			if (comment[1] == 'START') {
+			if (propertyName == 'START') {
 				// Subsequent lines belong to this tag.
 				inMultilineTags.push(tag);
-			} else if (comment[1] == 'END') {
+			} else if (propertyName == 'END') {
 				// Subsequent lines no longer belong to this tag.
 				inMultilineTags.splice(inMultilineTags.indexOf(tag), 1);
 				inTags.push(tag); // Still use ended tag on current line.
-			} else if (comment[1] == 'LINE') {
+			} else if (propertyName == 'LINE') {
 				// This line belongs to this tag.
 				inTags.push(tag);
-			} else { return console.error(`Unknown group designation "${comment[1]}".`); }
+			} else { return console.error(`Unknown group designation "${propertyName}".`); }
 		}
 
 		// Add tags to Line object.
@@ -191,9 +204,11 @@ const parseVersion = (script) => {
 
 		// Add Line object to list of lines.
 		lines.push(parsedLine);
-	});
+	}
 
-	tags.forEach((tag) => tag.size = getTagSize(lines, tag));
+	// Calculate size of each tag.
+	const requiredTag = tags.find((tag) => tag.name.toLowerCase() == REQUIRED_TAG_NAME.toLowerCase());
+	tags.forEach((tag) => tag.size = getTagSize(lines, tag, requiredTag));
 
 	// Make printable version of each Tag object's requirements.
 	for (let i = 0; i < tags.length; i++) {
@@ -212,34 +227,48 @@ const parseVersion = (script) => {
 	return chooseTags(lines, tags);
 }
 
-const getTagSize = (lines, tag) => {
+const getTagSize = (lines, tag, requiredTag) => {
+	// Get size of requirements with tag.
 	let script = '';
-	let tags = [...tag.requiresTags]; // Clone the array instead of referencing it to avoid adding the tag as its own requirement.
-	tags.push(tag);
+	let tags = [];
+	[...tag.requiresTags].concat([tag, requiredTag]).forEach((inclusiveTag) => fillTagsRecursively(inclusiveTag, tags));
 	lines.forEach((line) => {
 		if (line.tags.some((lineTag) => tags.includes(lineTag))) { script += `${line.code}\n`; }
 	});
 	let uglified = uglifyjs.minify(script);
-	if (uglified.error) { return console.error(`Error uglifying code: ${uglified.error}.\nMeasuring tag size using expanded code.`); } else { script = uglified.code; }
-	size = script.length;
+	if (uglified.error) { console.error(`Error uglifying code (inclusive). Measuring tag "${tag.name}" size using expanded code.\n${uglified.error}`); } else { script = uglified.code; }
+	let size = script.length;
 
-	// Get size of requirements without tag.
-	script = '';
-	tags = [...tag.requiresTags];
-	lines.forEach((line) => {
-		if (line.tags.some((lineTag) => tags.includes(lineTag))) { script += `${line.code}\n`; }
+	// Subtract size of requirements without tag.
+	if (tag != requiredTag) {
+		script = '';
+		tags = [];
+		[...tag.requiresTags].concat([requiredTag]).forEach((exclusiveTag) => fillTagsRecursively(exclusiveTag, tags));
+		lines.forEach((line) => {
+			if (line.tags.some((lineTag) => tags.includes(lineTag))) { script += `${line.code}\n`; }
+		});
+		uglified = uglifyjs.minify(script);
+		if (uglified.error) { console.error(`Error uglifying code (inclusive). Measuring tag "${tag.name}" size using expanded code.\n${uglified.error}`); } else { script = uglified.code; }
+		size -= script.length;
+	}
+
+	return size + 'B';
+}
+
+// Add tag requirements recursively.
+const fillTagsRecursively = (tag, array) => {
+	array.push(tag);
+	tag.requiresTags.forEach((tag) => {
+		if (array.includes(tag)) { return; }
+		fillTagsRecursively(tag, array);
 	});
-	uglified = uglifyjs.minify(script);
-	if (uglified.error) { return console.error(`Error uglifying code: ${uglified.error}.\nMeasuring tag size using expanded code.`); } else { script = uglified.code; }
-	size -= script.length;
-
-	return size + 'b';
 }
 
 const chooseTags = (lines, tags) => {
 	console.log(
-			`\n${columnify(tags, { columns: ['name', 'description', 'size', 'requires'] })}\n` +
-			'The REQUIRED tag is automatically included. Tag dependencies are automatically included.\n' +
+			'\n' +
+			`${columnify(tags, { columns: ['name', 'description', 'size', 'requires'] })}\n` +
+			`The ${REQUIRED_TAG_NAME} tag is automatically included. Tag dependencies are automatically included.\n` +
 			'Use an asterisk ("*") to include every tag.\n' +
 			'Use an exclamation point ("!") to include comments.'
 	);
@@ -247,19 +276,10 @@ const chooseTags = (lines, tags) => {
 	// Choose included tags from user input.
 	let usedTags = [];
 
-	// Add tag requirements recursively.
-	const addRequiredTag = (tag, array) => {
-		array.push(tag);
-		tag.requiresTags.forEach((tag) => {
-			if (array.includes(tag)) { return; }
-			addRequiredTag(tag, array);
-		});
-	}
-
 	// Add REQUIRED tag.
-	const requiredTag = tags.find((tag) => tag.name.toLowerCase() == 'REQUIRED'.toLowerCase());
-	if (!requiredTag) { return console.error('The REQUIRED tag is missing.'); }
-	addRequiredTag(requiredTag, usedTags);
+	const requiredTag = tags.find((tag) => tag.name.toLowerCase() == REQUIRED_TAG_NAME.toLowerCase());
+	if (!requiredTag) { return console.error(`The ${REQUIRED_TAG_NAME} tag is missing.`); }
+	fillTagsRecursively(requiredTag, usedTags);
 
 	const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 	rl.question('Included tags (space-separated): ', (input) => {
@@ -282,8 +302,8 @@ const chooseTags = (lines, tags) => {
 				includeComments = true;
 			} else {
 				const tag = tags.find((tag) => tag.name.toLowerCase() == tagName.toLowerCase());
-				if (!tag) { return console.log(`Unknown tag "${tagName}".`); }
-				addRequiredTag(tag, usedTags);
+				if (!tag) { return console.error(`Unknown tag "${tagName}".`); }
+				fillTagsRecursively(tag, usedTags);
 			}
 		});
 
@@ -298,17 +318,23 @@ const buildFile = (lines, tags, includeComments) => {
 	lines.forEach((line) => {
 		if (line.tags.some((tag) => tags.includes(tag))) {
 			output += line.code;
-			if (includeComments) { output += line.comment; }
+			if (includeComments && line.comment.length > 0) {
+				if (line.code.length > 0) { output += ' '; }
+				output += `// ${line.comment}`;
+			}
 			output += '\n';
 		}
 	});
 
 	// Uglify output.
-	const uglified = uglifyjs.minify(output);
-	if (uglified.error) { return console.error(`Error uglifying code: ${uglified.error}.\nOutputting expanded code.`); } else { output = uglified.code; }
+	if (!includeComments) {
+		const uglified = uglifyjs.minify(output);
+		if (uglified.error) { console.error(`Error uglifying code. Outputting expanded code.\n${uglified.error}`); } else { output = uglified.code; }
+	}
 
-	fs.writeFile('umbra.js', output, (err) => {
-		if (err) { return console.error(`Error while writing output file: ${err}.`); }
+	// Save to file.
+	fs.writeFile(LOCAL_FILE_NAME, output, (err) => {
+		if (err) { return console.error(`Error while writing output file:\n${err}.`); }
 	});
 
 	return true; // Success.
