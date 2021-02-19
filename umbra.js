4ef0fa2d82c5b5fd2370e024c40ad2fef5f3ea6b
Travis Martin <tjmartin2003@gmail.com>
2021-02-18 21:56:59 -0600

    WebGL utilities, canvas helper premade Components.

--- umbra.js
+++ umbra.js
@@ -25,7 +25,7 @@ class Umbra {
 
 		getComponentsRecursive(this.#scene)
 			.sort((a, b) => a.priority > b.priority ? 1 : -1)
-			.forEach((component) => component[event].call(component.gameObject));
+			.forEach((component) => component[event](this));
 	};
 
 	constructor(
@@ -33,9 +33,22 @@ class Umbra {
 		canvas = (() => {
 			const canvas = document.createElement('canvas');
 			canvas.style = 'touch-action:none;width:100%;height:100%;';
+			/*
+			 * CSS
+			 *
+			 * touch-action: none;
+			 * width: 100%;
+			 * height: 100%;
+			 */
 
-			document.body.style = 'margin:0;padding:0;';
 			document.body.appendChild(canvas);
+			document.body.style = 'margin:0;padding:0;';
+			/*
+			 * CSS
+			 *
+			 * margin: 0;
+			 * padding: 0;
+			 */
 
 			return canvas;
 		})(),
@@ -43,8 +56,8 @@ class Umbra {
 		// Updates per second for the fixed update loop.
 		ups = 30
 	) {
-		this.cnv = canvas;
-		this.ctx = this.cnv.getContext('webgl');
+		this.canvas = canvas;
+		this.gl = this.canvas.getContext('webgl2');
 
 		// Update loop; variable frequency.
 		const update = () => {
@@ -105,9 +118,9 @@ class GameObject {
 class Component {
 	// Enumeration of events.
 	static events = {
-		LOAD: 'load', // Called when the Scene containing the GameObject this Component belongs to is loaded.
-		UPDATE: 'update', // Called on each animation frame; varies in frequency based on lag.
-		FIXED: 'fixed' // Called by a fixed timer; 30 times per second by default.
+		LOAD: 'l', // Called when the Scene containing the GameObject this Component belongs to is loaded.
+		UPDATE: 'u', // Called on each animation frame; varies in frequency based on lag.
+		FIXED: 'f' // Called by a fixed timer; 30 times per second by default.
 	};
 
 	#gameObject;
@@ -122,4 +135,62 @@ class Component {
 	get gameObject() {
 		return this.#gameObject;
 	}
+}
+
+class DrawingBufferResizer extends Component {
+	constructor(gameObject, priority = -1) {
+		super(gameObject, priority);
+
+		this[Component.events.UPDATE] = (umbra) => {
+			const canvas = umbra.canvas;
+
+			const displayWidth = canvas.clientWidth;
+			const displayHeight = canvas.clientHeight;
+
+			if (canvas.width != displayWidth || canvas.height != displayHeight) {
+				canvas.width = displayWidth;
+				canvas.height = displayHeight;
+			}
+		};
+	}
+}
+
+class Background extends Component {
+	constructor(gameObject, r = 0, g = 0, b = 0, a = 1, priority = -1) {
+		super(gameObject, priority);
+
+		this[Component.events.LOAD] = (umbra) => umbra.gl.clearColor(r, g, b, a);
+		this[Component.events.UPDATE] = (umbra) => umbra.gl.clear(umbra.gl.COLOR_BUFFER_BIT);
+	}
+}
+
+class WebGLUtility {
+	static createShader = (gl, type, src) => {
+		const shader = gl.createShader(type);
+		gl.shaderSource(shader, src);
+		gl.compileShader(shader);
+
+		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+			console.error(`Failed to create shader: ${gl.getShaderInfoLog(shader)}`);
+			gl.deleteShader(shader);
+			return;
+		}
+
+		return shader;
+	};
+
+	static createProgram = (gl, vertexShader, fragmentShader) => {
+		const program = gl.createProgram();
+		gl.attachShader(program, vertexShader);
+		gl.attachShader(program, fragmentShader);
+		gl.linkProgram(program);
+
+		if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+			console.error(`Failed to create program: ${gl.getProgramInfoLog(program)}`);
+			gl.deleteProgram(program);
+			return;
+		}
+
+		return program;
+	};
 }
