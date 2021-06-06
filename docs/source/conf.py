fa0629d25dbe6d966b2501420f264f444aaa2b5d
Travis Martin <tjmartin2003@gmail.com>
2021-06-05 23:24:28 -0500

    Package updates.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 project = 'Umbra Web'
 copyright = '2021, Travis Martin'
 author = 'Travis Martin'
-release = '0.0.2'
+release = '0.0.3'
 extensions = ['sphinx_js']
 html_theme = 'sphinx_rtd_theme'
 primary_domain = 'js'
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @file Lightweight JavaScript game framework.
- * @version 0.0.2
+ * @version 0.0.3
  * @author Travis Martin
  * @copyright Travis Martin 2021
  * @license MIT
