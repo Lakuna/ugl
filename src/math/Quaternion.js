d49b36f9e8c027b3a6fd5043ca154f31761558c8
Travis Martin <tjmartin2003@gmail.com>
2021-06-19 15:25:24 -0500

    Quaternion extends Vector. Quaternion.prototype.conjugate(), .divide(), .sphericalLinearInterpolation().

--- src/math/Quaternion.js
+++ src/math/Quaternion.js
@@ -1,37 +1,18 @@
 import { degreesToRadians } from "./degreesToRadians.js";
 import { Vector } from "./Vector.js";
 
-/*
-TODO: Remove references.
-https://blog.hamaluik.ca/posts/quaternions-as-four-dimensional-complex-numbers/
-https://arxiv.org/ftp/arxiv/papers/2009/2009.00425.pdf
-https://www.sciencedirect.com/topics/computer-science/quaternion-multiplication
-https://github.com/infusion/Quaternion.js/blob/master/quaternion.js
-*/
-
-export class Quaternion extends Array {
+export class Quaternion extends Vector {
 	constructor(...data) {
 		super(...(data.length ? data : Quaternion.identity())); // Default to identity.
 	}
 
-	set(...data) {
-		while (this.length > 0) { this.pop(); }
-		for (const value of data) { this.push(value); }
-
-		return this;
-	}
-
-	setAngle(axis, degrees) {
-		const radians = degreesToRadians(degrees) * 0.5;
-		this[0] = Math.cos(radians);
-		const sine = Math.sin(radians);
-		this[1] = sine * axis[0];
-		this[2] = sine * axis[1];
-		this[3] = sine * axis[2];
-		return this;
+	conjugate() {
+		this.set(this[0], -this[1], -this[2], -this[3]);
 	}
 
 	multiply(quaternion) {
+		// Algorithm from Quaternion.js: https://github.com/infusion/Quaternion.js
+
 		// Real scalar values.
 		const w1 = this[0];
 		const w2 = quaternion[0];
@@ -49,6 +30,20 @@ export class Quaternion extends Array {
 		);
 	}
 
+	divide(quaternion) {
+		return this.multiply(new Quaternion(...quaternion).invert());
+	}
+
+	setAngle(axis, degrees) {
+		const radians = degreesToRadians(degrees) * 0.5;
+		this[0] = Math.cos(radians);
+		const sine = Math.sin(radians);
+		this[1] = sine * axis[0];
+		this[2] = sine * axis[1];
+		this[3] = sine * axis[2];
+		return this;
+	}
+
 	rotateX(degrees) {
 		return this.multiply(new Quaternion().setAngle([1, 0, 0], degrees));
 	}
@@ -61,13 +56,20 @@ export class Quaternion extends Array {
 		return this.multiply(new Quaternion().setAngle([0, 0, 1], degrees));
 	}
 
-	normalize() {
-		return this.set(new Vector(...this).normalize());
+	sphericalLinearInterpolation(quaternion, t) {
+		let cosom = this.dot(quaternion);
+		if (cosom < 0) {
+			cosom = -cosom;
+			quaternion = new Quaternion(quaternion).negate();
+		}
+
+		const omega = Math.acos(cosom);
+		const sinom = Math.sin(omega);
+
+		const scale0 = Math.sin((1 - t) * omega) / sinom;
+		const scale1 = Math.sin(t * omega) / sinom;
+
+		return this.operate(quaternion, (a, b) => scale0 * a + scale1 * b);
 	}
 }
-Quaternion.fromRule = (length, rule) => {
-	let data = [];
-	for (let i = 0; i < length; i++) { data[i] = rule(i); }
-	return new Quaternion(...data);
-};
 Quaternion.identity = () => new Quaternion(1, 0, 0, 0);
