5ec8a4852518779ac45c5d7fb8584e81684df2fd
Travis Martin <tjmartin2003@gmail.com>
2021-06-21 10:55:17 -0500

    Euler.toQuaternion().

--- src/math/Euler.js
+++ src/math/Euler.js
@@ -1,8 +1,22 @@
 import { Matrix } from "./Matrix.js";
+import { Quaternion } from "./Quaternion.js";
 
 export class Euler extends Array {
 	toQuaternion() {
-		return this.toMatrix().toQuaternion();
+		// return this.toMatrix().toQuaternion();
+
+		const sinX = Math.sin(this[0] * 0.5);
+		const cosX = Math.cos(this[0] * 0.5);
+		const sinY = Math.sin(this[1] * 0.5);
+		const cosY = Math.cos(this[1] * 0.5);
+		const sinZ = Math.sin(this[2] * 0.5);
+		const cosZ = Math.cos(this[2] * 0.5);
+
+		return new Quaternion(
+			sinX * cosY * cosZ + cosX * sinY * sinZ,
+			cosX * sinY * cosZ - sinX * cosY * sinZ,
+			cosX * cosY * sinZ + sinX * sinY * cosZ,
+			cosX * cosY * cosZ - sinX * sinY * sinZ);
 	}
 
 	toMatrix() {
