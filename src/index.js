0ae17c07bc43a34429632228282c52b58c01f0bd
Travis Martin <tjmartin2003@gmail.com>
2021-06-21 14:46:22 -0500

    Remove most code to debug Bundlephobia issue.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 // Math
 export * from "./math/Color.js";
-export * from "./math/Euler.js";
-export * from "./math/Matrix.js";
-export * from "./math/Quaternion.js";
-export * from "./math/Vector.js";
