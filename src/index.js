d65743b937eb1b842d81d570b07eca5fd0778847
Travis Martin <tjmartin2003@gmail.com>
2021-06-30 01:41:09 -0500

    Change browser target back to unbundled source.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // Math
-export { Color }		from "./math/Color.js";
-export { Euler }		from "./math/Euler.js";
-export { Matrix }		from "./math/Matrix.js";
-export { Quaternion }	from "./math/Quaternion.js";
-export { Vector }		from "./math/Vector.js";
+export * from "./math/Color.js";
+export * from "./math/Euler.js";
+export * from "./math/Matrix.js";
+export * from "./math/Quaternion.js";
+export * from "./math/Vector.js";
 
 // WebGL
-export { Program }		from "./webgl/Program.js";
-export { Renderer }		from "./webgl/Renderer.js";
-export { Shader }		from "./webgl/Shader.js";
-export { Transform }	from "./webgl/Transform.js";
+export * from "./webgl/Program.js";
+export * from "./webgl/Renderer.js";
+export * from "./webgl/Shader.js";
+export * from "./webgl/Transform.js";
