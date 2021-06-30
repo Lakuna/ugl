02ad385ec09d93a0c025289a13d3fe4b3a1e87ad
Travis Martin <tjmartin2003@gmail.com>
2021-06-29 22:29:57 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // Math
-export * from "./math/Color.js";
-export * from "./math/Euler.js";
-export * from "./math/Matrix.js";
-export * from "./math/Quaternion.js";
-export * from "./math/Vector.js";
+export { Color }		from "./math/Color.js";
+export { Euler }		from "./math/Euler.js";
+export { Matrix }		from "./math/Matrix.js";
+export { Quaternion }	from "./math/Quaternion.js";
+export { Vector }		from "./math/Vector.js";
 
 // WebGL
-export * from "./webgl/Program.js";
-export * from "./webgl/Renderer.js";
-export * from "./webgl/Shader.js";
+export { Program }		from "./webgl/Program.js";
+export { Renderer }		from "./webgl/Renderer.js";
+export { Shader }		from "./webgl/Shader.js";
+export { Transform }	from "./webgl/Transform.js";
