b5bedd020a4244b793241b2f035dc5b30612dfd9
Travis Martin <tjmartin2003@gmail.com>
2021-07-28 16:24:16 -0500

    Vector letter values.

--- src/math/Vector.js
+++ src/math/Vector.js
@@ -13,6 +13,38 @@ export class Vector extends Array {
 		return Math.sqrt(sigma(0, this.length - 1, (n) => this[n] ** 2));
 	}
 
+	get x() {
+		return this[0] ?? 0;
+	}
+
+	set x(value) {
+		this[0] = value;
+	}
+
+	get y() {
+		return this[1] ?? 0;
+	}
+
+	set y(value) {
+		this[1] = value;
+	}
+
+	get z() {
+		return this[2] ?? 0;
+	}
+
+	set z(value) {
+		this[2] = value;
+	}
+
+	get w() {
+		return this[3] ?? 0;
+	}
+
+	set w(value) {
+		this[3] = value;
+	}
+
 	distance(vector) {
 		let output = 0;
 		for (let i = 0; i < vector.length; i++) {
