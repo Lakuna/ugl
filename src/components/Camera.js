32b3b38eb7751398528a72ece5e4fa80a6ef0e94
Travis Martin <tjmartin2003@gmail.com>
2021-08-10 00:21:55 -0500

    Switch to ESDoc-Next

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,33 +1,109 @@
 import { Transform } from "./Transform.js";
 import { Matrix } from "../math/Matrix.js";
 
+/** Class representing a camera. */
 export class Camera extends Transform {
-	constructor({ near = 0.1, far = 100, fieldOfView = 45, aspectRatio = 1, left, right, bottom, top, zoom = 1 } = {}) {
+	/**
+	 * Create a camera.
+	 * @param {Object} [arguments] - An object containing the arguments.
+	 * @param {number} [arguments.near=0.1] - The nearest that the camera can see.
+	 * @param {number} [arguments.far=100] - The farthest that the camera can see.
+	 * @param {number} [arguments.fieldOfView=45 * (Math.PI / 180)] - The field of view of the camera in radians.
+	 * @param {number} [arguments.aspectRatio=1] - The aspect ratio of the output.
+	 * @param {number} [arguments.left] - The left boundary of the output. Makes the camera orthographic if given a value.
+	 * @param {number} [arguments.right] - The right boundary of the output. Makes the camera orthographic if given a value.
+	 * @param {number} [arguments.bottom] - The bottom boundary of the output. Makes the camera orthographic if given a value.
+	 * @param {number} [arguments.top] - The top boundary of the output. Makes the camera orthographic if given a value.
+	 * @param {number} [arguments.zoom=1] - The zoom applied to the camera if it is orthographic.
+	 */
+	constructor({ near = 0.1, far = 100, fieldOfView = 45 * (Math.PI / 180), aspectRatio = 1, left, right, bottom, top, zoom = 1 } = {}) {
 		super();
 
-		Object.defineProperties(this, {
-			near: { value: near, writable: true },
-			far: { value: far, writable: true },
-			fieldOfView: { value: fieldOfView, writable: true },
-			aspectRatio: { value: aspectRatio, writable: true },
-			left: { value: left, writable: true },
-			right: { value: right, writable: true },
-			bottom: { value: bottom, writable: true },
-			top: { value: top, writable: true },
-			zoom: { value: zoom, writable: true }
-		});
+		/**
+		 * The nearest that the camera can see.
+		 * @type {number}
+		 */
+		this.near = near;
+
+		/**
+		 * The farthest that the camera can see.
+		 * @type {number}
+		 */
+		this.far = far;
+
+		/**
+		 * The field of view of the camera in radians.
+		 * @type {number}
+		 */
+		this.fieldOfView = fieldOfView;
+
+		/**
+		 * The aspect ratio of the output.
+		 * @type {number}
+		 */
+		this.aspectRatio = aspectRatio;
+
+		/**
+		 * The left boundary of the output. Makes the camera orthographic if given a value.
+		 * @type {number}
+		 */
+		this.left = left;
+
+		/**
+		 * The right boundary of the output. Makes the camera orthographic if given a value.
+		 * @type {number}
+		 */
+		this.right = right;
+
+		/**
+		 * The bottom boundary of the output. Makes the camera orthographic if given a value.
+		 * @type {number}
+		 */
+		this.bottom = bottom;
+
+		/**
+		 * The top boundary of the output. Makes the camera orthographic if given a value.
+		 * @type {number}
+		 */
+		this.top = top;
+
+		/**
+		 * The zoom applied to the camera if it is orthographic.
+		 * @type {number}
+		 */
+		this.zoom = zoom;
 	}
 
+	/**
+	 * Whether this camera is orthographic. Read-only.
+	 * @type {boolean}
+	 */
+	get orthographic() {
+		return this.left || this.right || this.bottom || this.top;
+	}
+
+	/**
+	 * The projection matrix of the camera. Read-only.
+	 * @type {Matrix}
+	 */
 	get projectionMatrix() {
-		return this.left || this.right || this.bottom || this.top
+		return this.orthographic
 			? new Matrix().orthographic(this.left / this.zoom, this.right / this.zoom, this.bottom / this.zoom, this.top / this.zoom, this.near, this.far)
 			: new Matrix().perspective(this.fieldOfView, this.aspectRatio, this.near, this.far);
 	}
 
+	/**
+	 * The view matrix of the camera. Read-only.
+	 * @type {Matrix}
+	 */
 	get viewMatrix() {
 		return this.worldMatrix().invert();
 	}
 
+	/**
+	 * The projection view matrix of the camera. Read-only.
+	 * @type {Matrix}
+	 */
 	get projectionViewMatrix() {
 		return this.projectionMatrix.multiply(this.viewMatrix);
 	}
