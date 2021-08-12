28563483d85ba5355215f06abd25cd752fe70730
Travis Martin <tjmartin2003@gmail.com>
2021-08-12 02:05:42 -0500

    Recursive fullscreen to fix makeFullscreenCanvas() in code playgrounds.

--- src/utility/makeFullscreenCanvas.js
+++ src/utility/makeFullscreenCanvas.js
@@ -6,24 +6,37 @@ export const makeFullscreenCanvas = () => {
 	if (typeof document == "undefined") { throw new Error("Cannot create a canvas in a headless environment."); }
 
 	const canvas = document.createElement("canvas");
-	canvas.style = "touch-action:none;width:100%;height:100%";
+	canvas.style = "touch-action:none;";
 	/* CSS
 	canvas {
 		touch-action: none;
-		width: 100%;
-		height: 100%;
 	}
 	*/
 
 	document.body = document.createElement("body"); // Clear document body.
 	document.body.appendChild(canvas);
-	document.body.style = "margin:0;height:100%";
+	document.body.style = "margin:0;";
 	/* CSS
 	body {
 		margin: 0px;
-		height: 100%;
 	}
 	*/
+
+	// Recursively make all parents of the canvas fullscreen.
+	const recursiveFullscreen = (element) => {
+		element.style += "width:100%;height:100%";
+		/* CSS
+		* {
+			width: 100%;
+			height: 100%;
+		}
+		*/
+
+		if (element.parentElement) {
+			recursiveFullscreen(element.parentElement);
+		}
+	}
+	recursiveFullscreen(canvas);
 
 	return canvas;
 };
