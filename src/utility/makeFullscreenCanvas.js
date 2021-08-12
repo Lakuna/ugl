516faf981d80b69f934a83a71ad9e66e0c70d812
Travis Martin <tjmartin2003@gmail.com>
2021-08-12 01:48:28 -0500

    makeFullscreenCanvas fix.

--- src/utility/makeFullscreenCanvas.js
+++ src/utility/makeFullscreenCanvas.js
@@ -6,23 +6,24 @@ export const makeFullscreenCanvas = () => {
 	if (typeof document == "undefined") { throw new Error("Cannot create a canvas in a headless environment."); }
 
 	const canvas = document.createElement("canvas");
-	canvas.style = "touch-action:none;width:100%;height:100%";
+	
+
+	const style = document.createElement("style");
+	style.innerHTML = "canvas{touch-action:none;width:100%;height:100%}body{margin:0}";
 	/* CSS
 	canvas {
 		touch-action: none;
 		width: 100%;
 		height: 100%;
 	}
-	*/
-
-	document.body = document.createElement("body"); // Clear document body.
-	document.body.appendChild(canvas);
-	document.body.style = "margin:0";
-	/* CSS
 	body {
 		margin: 0px;
 	}
 	*/
+
+	document.body = document.createElement("body"); // Clear document body.
+	document.body.appendChild(canvas);
+	document.body.appendChild(style);
 
 	return canvas;
 };
