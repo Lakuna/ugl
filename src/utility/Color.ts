3ed22aac88ed9dc48faa5b7bfda342511372e20c
Travis Martin <tjmartin2003@gmail.com>
2022-04-25 20:54:32 -0500

    Update Color to match Vector.

--- src/utility/Color.ts
+++ src/utility/Color.ts
@@ -1,5 +1,5 @@
 /** A color. */
-export class Color extends Float32Array {
+export class Color extends Array<number> implements ReadonlyArray<number> {
 	/**
 	 * Creates a color from a hexadecimal value.
 	 * @param hex - The color as a hexadecimal number.
@@ -16,44 +16,12 @@ export class Color extends Float32Array {
 	constructor(r: number, g: number, b: number, a?: number);
 
 	constructor(r = 0xFFFFFF, g?: number, b?: number, a?: number) {
-		super(g ? [r, g, b as number, a as number] : [
+		super(...(g ? [r, g, b as number, a as number] : [
 			((r >> 16) & 0xFF) / 0xFF,
 			((r >> 8) & 0xFF) / 0xFF,
 			(r & 0xFF) / 0xFF,
 			0xFF / 0xFF
-		]);
-	}
-
-	/** The red value of this color. */
-	get r(): number {
-		return this[0] as number;
-	}
-	set r(value: number) {
-		this[0] = value;
-	}
-
-	/** The green value of this color. */
-	get g(): number {
-		return this[1] as number;
-	}
-	set g(value: number) {
-		this[1] = value;
-	}
-
-	/** The blue value of this color. */
-	get b(): number {
-		return this[2] as number;
-	}
-	set b(value: number) {
-		this[2] = value;
-	}
-
-	/** The alpha value of this color. */
-	get a(): number {
-		return this[3] as number;
-	}
-	set a(value: number) {
-		this[3] = value;
+		]));
 	}
 
 	/** The luminance of this color. */
