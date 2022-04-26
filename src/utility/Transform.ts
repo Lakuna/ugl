9dc3817a15e6b045f6c122a395f0ca8781e6fe6d
Travis Martin <tjmartin2003@gmail.com>
2022-04-25 21:16:03 -0500

    Add origin to transformation matrix calculation.

--- src/utility/Transform.ts
+++ src/utility/Transform.ts
@@ -16,6 +16,7 @@ export class Transform extends Component {
     this.translation = new Vector(0, 0, 0);
     this.rotation = new Vector(0, 0, 0);
     this.scale = new Vector(1, 1, 1);
+    this.origin = new Vector(0, 0, 0);
   }
 
   /** The translation of this transform. */
@@ -27,16 +28,26 @@ export class Transform extends Component {
   /** The scale of this transform. */
   scale: Vector;
 
+  /** The origin for this transform to rotate and scale about. */
+  origin: Vector;
+
   /** The target for this transform to rotate towards. */
   target: Vector | undefined;
 
   /** The transformation matrix of this transform relative to its parent. */
   get matrix(): Matrix {
-    return (this.target
-      ? Matrix.fromLookAt(this.translation, this.target, new Vector(0, 1, 0))
-      : Matrix.fromTranslation(this.translation?.[0] ?? 0, this.translation?.[1] ?? 0,this.translation?.[2] ?? 0)
-        .rotate(this.rotation?.[0] ?? 0, this.rotation?.[1] ?? 0,this.rotation?.[2] ?? 0))
-      .scale(this.scale?.[0] ?? 0, this.scale?.[1] ?? 0,this.scale?.[2] ?? 0);
+    return this.target
+      ? new Matrix()
+        .lookAt(this.translation, this.target, new Vector(0, 1, 0))
+        .translate(this.origin?.[0] ?? 0, this.origin?.[1] ?? 0, this.origin?.[2] ?? 0)
+        .scale(this.scale?.[0] ?? 0, this.scale?.[1] ?? 0,this.scale?.[2] ?? 0)
+        .translate(-(this.origin?.[0] ?? 0), -(this.origin?.[1] ?? 0), -(this.origin?.[2] ?? 0))
+      : new Matrix()
+        .translate(this.translation?.[0] ?? 0, this.translation?.[1] ?? 0, this.translation?.[2] ?? 0)
+        .translate(this.origin?.[0] ?? 0, this.origin?.[1] ?? 0, this.origin?.[2] ?? 0)
+        .rotate(this.rotation?.[0] ?? 0, this.rotation?.[1] ?? 0,this.rotation?.[2] ?? 0)
+        .scale(this.scale?.[0] ?? 0, this.scale?.[1] ?? 0,this.scale?.[2] ?? 0)
+        .translate(-(this.origin?.[0] ?? 0), -(this.origin?.[1] ?? 0), -(this.origin?.[2] ?? 0));
   }
 
   /** The transform component of the parent object of the object that this transform is attached to. */
