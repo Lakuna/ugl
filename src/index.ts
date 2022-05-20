82b8d7e1e1311caa69468f9cfd11888efa716f15
Travis Martin <tjmartin2003@gmail.com>
2022-05-19 22:38:42 -0500

    Index update didn't push

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ export * from "./math/Matrix.js";
 export * from "./math/Matrix2.js";
 export * from "./math/Matrix3.js";
 export * from "./math/Matrix4.js";
+export * from "./math/Quaternion.js";
+export * from "./math/SquareMatrix.js";
 export * from "./math/Vector.js";
 export * from "./math/Vector2.js";
 export * from "./math/Vector3.js";
