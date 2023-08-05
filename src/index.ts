af48c363d115f32bbf11f3592f77af6c77147366
Travis Martin <tjmartin2003@gmail.com>
2023-08-05 03:59:27 -0500

    Shader and shader program error message improvements.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export type { default as Box } from "#types/Box";
 export type { default as MeasuredIterable } from "#types/MeasuredIterable";
 export type { FloatTypedArray, IntTypedArray, TypedArray, UintTypedArray } from "#types/TypedArray";
 export { default as Color, type ColorLike } from "#utility/Color";
+export { default as ProgramLinkError } from "#utility/ProgramLinkError";
+export { default as ShaderCompileError } from "#utility/ShaderCompileError";
 export { default as Cubemap, CubemapMip } from "#textures/Cubemap";
 export {
 	default as Texture, Mip, Mipmap, type MipSource, MipmapTarget, TextureBaseFormat,
