3b77aa6ae6c1f947460b538082f44cde354c39ba
Travis Martin <tjmartin2003@gmail.com>
2023-08-15 19:24:03 -0500

    `UniformFactory` and `AttributeFactory`.

--- src/variables/attributes/Attribute.ts
+++ src/variables/attributes/Attribute.ts
@@ -1,10 +1,6 @@
 import Variable from "#Variable";
 import type Program from "#Program";
 import UnsupportedOperationError from "#UnsupportedOperationError";
-import AttributeType from "#AttributeType";
-import FloatAttribute from "#FloatAttribute";
-import IntegerAttribute from "#IntegerAttribute";
-import MatrixAttribute from "#MatrixAttribute";
 import type BufferInfo from "#BufferInfo";
 import BufferTarget from "#BufferTarget";
 import BufferTargetError from "#BufferTargetError";
@@ -14,45 +10,6 @@ import BufferTargetError from "#BufferTargetError";
  * @see [Tutorial](https://www.lakuna.pw/a/webgl/attributes)
  */
 export default abstract class Attribute extends Variable {
-	/**
-	 * Creates an attribute for the given variable type.
-	 * @param program The shader program that this attribute belongs to.
-	 * @param index The index of this attribute.
-	 */
-	public static create(program: Program, index: number): Attribute {
-		const activeInfo: WebGLActiveInfo | null = program.context.internal.getActiveAttrib(program.internal, index);
-		if (!activeInfo) { throw new UnsupportedOperationError(); }
-
-		switch (activeInfo.type as AttributeType) {
-			case AttributeType.FLOAT:
-			case AttributeType.FLOAT_VEC2:
-			case AttributeType.FLOAT_VEC3:
-			case AttributeType.FLOAT_VEC4:
-				return new FloatAttribute(program, index);
-			case AttributeType.INT:
-			case AttributeType.INT_VEC2:
-			case AttributeType.INT_VEC3:
-			case AttributeType.INT_VEC4:
-			case AttributeType.UNSIGNED_INT:
-			case AttributeType.UNSIGNED_INT_VEC2:
-			case AttributeType.UNSIGNED_INT_VEC3:
-			case AttributeType.UNSIGNED_INT_VEC4:
-			case AttributeType.BOOL:
-			case AttributeType.BOOL_VEC2:
-			case AttributeType.BOOL_VEC3:
-			case AttributeType.BOOL_VEC4:
-				return new IntegerAttribute(program, index);
-			case AttributeType.FLOAT_MAT2:
-				return new MatrixAttribute(program, index, 2);
-			case AttributeType.FLOAT_MAT3:
-				return new MatrixAttribute(program, index, 3);
-			case AttributeType.FLOAT_MAT4:
-				return new MatrixAttribute(program, index, 4);
-			default:
-				throw new UnsupportedOperationError();
-		}
-	}
-
 	/**
 	 * Creates an attribute.
 	 * @param program The shader program that this attribute belongs to.
--- src/variables/uniforms/Uniform.ts
+++ src/variables/uniforms/Uniform.ts
@@ -1,29 +1,6 @@
 import Variable from "#Variable";
 import type Program from "#Program";
 import UnsupportedOperationError from "#UnsupportedOperationError";
-import UniformType from "#UniformType";
-import FloatUniform from "#FloatUniform";
-import FloatVector2Uniform from "#FloatVector2Uniform";
-import FloatVector3Uniform from "#FloatVector3Uniform";
-import FloatVector4Uniform from "#FloatVector4Uniform";
-import SamplerUniform from "#SamplerUniform";
-import IntegerUniform from "#IntegerUniform";
-import IntegerVector2Uniform from "#IntegerVector2Uniform";
-import IntegerVector3Uniform from "#IntegerVector3Uniform";
-import IntegerVector4Uniform from "#IntegerVector4Uniform";
-import UnsignedIntegerUniform from "#UnsignedIntegerUniform";
-import UnsignedIntegerVector2Uniform from "#UnsignedIntegerVector2Uniform";
-import UnsignedIntegerVector3Uniform from "#UnsignedIntegerVector3Uniform";
-import UnsignedIntegerVector4Uniform from "#UnsignedIntegerVector4Uniform";
-import FloatMatrix2x2Uniform from "#FloatMatrix2x2Uniform";
-import FloatMatrix2x3Uniform from "#FloatMatrix2x3Uniform";
-import FloatMatrix2x4Uniform from "#FloatMatrix2x4Uniform";
-import FloatMatrix3x2Uniform from "#FloatMatrix3x2Uniform";
-import FloatMatrix3x3Uniform from "#FloatMatrix3x3Uniform";
-import FloatMatrix3x4Uniform from "#FloatMatrix3x4Uniform";
-import FloatMatrix4x2Uniform from "#FloatMatrix4x2Uniform";
-import FloatMatrix4x3Uniform from "#FloatMatrix4x3Uniform";
-import FloatMatrix4x4Uniform from "#FloatMatrix4x4Uniform";
 import type MeasuredIterable from "#MeasuredIterable";
 import type { UniformValue } from "#UniformValue";
 
@@ -32,80 +9,6 @@ import type { UniformValue } from "#UniformValue";
  * @see [Tutorial](https://www.lakuna.pw/a/webgl/uniforms)
  */
 export default abstract class Uniform extends Variable {
-	/**
-	 * Creates a uniform for the given variable type.
-	 * @param program The shader program that this uniform belongs to.
-	 * @param index The index of this uniform.
-	 * @param textureUnit The texture unit to assign to this uniform if it ends up being a sampler.
-	 */
-	public static create(program: Program, index: number, textureUnit: number): Uniform {
-		const activeInfo: WebGLActiveInfo | null = program.context.internal.getActiveUniform(program.internal, index);
-		if (!activeInfo) { throw new UnsupportedOperationError(); }
-
-		switch (activeInfo.type as UniformType) {
-			case UniformType.FLOAT:
-				return new FloatUniform(program, index);
-			case UniformType.FLOAT_VEC2:
-				return new FloatVector2Uniform(program, index);
-			case UniformType.FLOAT_VEC3:
-				return new FloatVector3Uniform(program, index);
-			case UniformType.FLOAT_VEC4:
-				return new FloatVector4Uniform(program, index);
-			case UniformType.SAMPLER_2D:
-			case UniformType.SAMPLER_3D:
-			case UniformType.SAMPLER_CUBE:
-			case UniformType.SAMPLER_2D_SHADOW:
-			case UniformType.SAMPLER_2D_ARRAY:
-			case UniformType.SAMPLER_2D_ARRAY_SHADOW:
-			case UniformType.SAMPLER_CUBE_SHADOW:
-			case UniformType.INT_SAMPLER_2D:
-			case UniformType.INT_SAMPLER_3D:
-			case UniformType.INT_SAMPLER_CUBE:
-			case UniformType.INT_SAMPLER_2D_ARRAY:
-				return new SamplerUniform(program, index, textureUnit);
-			case UniformType.BOOL:
-			case UniformType.INT:
-				return new IntegerUniform(program, index);
-			case UniformType.BOOL_VEC2:
-			case UniformType.INT_VEC2:
-				return new IntegerVector2Uniform(program, index);
-			case UniformType.BOOL_VEC3:
-			case UniformType.INT_VEC3:
-				return new IntegerVector3Uniform(program, index);
-			case UniformType.BOOL_VEC4:
-			case UniformType.INT_VEC4:
-				return new IntegerVector4Uniform(program, index);
-			case UniformType.UNSIGNED_INT:
-				return new UnsignedIntegerUniform(program, index);
-			case UniformType.UNSIGNED_INT_VEC2:
-				return new UnsignedIntegerVector2Uniform(program, index);
-			case UniformType.UNSIGNED_INT_VEC3:
-				return new UnsignedIntegerVector3Uniform(program, index);
-			case UniformType.UNSIGNED_INT_VEC4:
-				return new UnsignedIntegerVector4Uniform(program, index);
-			case UniformType.FLOAT_MAT2:
-				return new FloatMatrix2x2Uniform(program, index);
-			case UniformType.FLOAT_MAT3:
-				return new FloatMatrix3x3Uniform(program, index);
-			case UniformType.FLOAT_MAT4:
-				return new FloatMatrix4x4Uniform(program, index);
-			case UniformType.FLOAT_MAT2x3:
-				return new FloatMatrix2x3Uniform(program, index);
-			case UniformType.FLOAT_MAT2x4:
-				return new FloatMatrix2x4Uniform(program, index);
-			case UniformType.FLOAT_MAT3x2:
-				return new FloatMatrix3x2Uniform(program, index);
-			case UniformType.FLOAT_MAT3x4:
-				return new FloatMatrix3x4Uniform(program, index);
-			case UniformType.FLOAT_MAT4x2:
-				return new FloatMatrix4x2Uniform(program, index);
-			case UniformType.FLOAT_MAT4x3:
-				return new FloatMatrix4x3Uniform(program, index);
-			default:
-				throw new UnsupportedOperationError();
-		}
-	}
-
 	/**
 	 * Creates a uniform.
 	 * @param program The shader program that this uniform belongs to.
