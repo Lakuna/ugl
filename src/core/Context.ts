ccfd90ef24446d9161145c97720d11ddb13000f3
Travis Martin <tjmartin2003@gmail.com>
2024-07-21 01:55:28 -0500

    Add some TODOs.

--- src/core/Context.ts
+++ src/core/Context.ts
@@ -1,4 +1,6 @@
 // TODO: Document which functions are capable of throwing errors and why.
+// TODO: Ensure that parameters are never set over their limits; warn if set over the common level of support: https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/WebGL_best_practices#understand_system_limits.
+// TODO: Remove various checks for production builds: https://webpack.js.org/plugins/define-plugin/.
 
 import {
 	ACTIVE_TEXTURE,
