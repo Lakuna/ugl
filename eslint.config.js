1318ddae554825f1fac77e89254797755063ab7f
Travis Martin <tjmartin2003@gmail.com>
2026-02-18 10:43:30 -0600

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,68 +4,6 @@ import prettier from "eslint-plugin-prettier/recommended";
 import tsdoc from "eslint-plugin-tsdoc";
 import tseslint from "typescript-eslint";
 
-/*
-// Old
-export default defineConfig(
-	// Enable all ESLint rules.
-	eslint.configs.all,
-
-	// Disable specific rules.
-	{
-		rules: {
-			// Possible Problems
-			// N/A
-
-			// Suggestions
-			complexity: "off",
-			"id-length": "off",
-			"max-depth": "off",
-			"max-lines": "off",
-			"max-lines-per-function": "off",
-			"max-nested-callbacks": "off",
-			"max-params": "off",
-			"max-statements": "off",
-			"no-bitwise": "off",
-			"no-continue": "off",
-			"no-div-regex": "off",
-			"no-inline-comments": "off",
-			"no-label-var": "off",
-			"no-labels": "off",
-			"no-magic-numbers": "off",
-			"no-nested-ternary": "off",
-			"no-plusplus": "off",
-			"no-shadow": "off", // Must use `@typescript-eslint/no-shadow` instead.
-			"no-ternary": "off",
-			"no-undef-init": "off",
-			"no-void": "off",
-			"no-warning-comments": "off",
-			"one-var": ["error", "never"]
-
-			// Layout and Formatting
-			// N/A
-		}
-	},
-
-	// Enable type checking and related rules.
-	...configs.strictTypeChecked,
-	...configs.stylisticTypeChecked,
-	{
-		languageOptions: {
-			parser,
-			parserOptions: { ecmaVersion: "latest", project: true }
-		},
-		plugins: { "@typescript-eslint": plugin },
-		rules: { "@typescript-eslint/no-shadow": "error" }
-	},
-
-	// Enable the TSDoc plugin.
-	{ plugins: { tsdoc }, rules: { "tsdoc/syntax": "error" } },
-
-	// Enable the Prettier plugin.
-	prettier // Includes `eslint-config-prettier` and `eslint-plugin-prettier`.
-);
-*/
-
 export default defineConfig(
 	eslint.configs.all,
 	tseslint.configs.all,
@@ -90,7 +28,8 @@ export default defineConfig(
 				{ format: ["strictCamelCase", "StrictPascalCase"], selector: "import" },
 				{ format: ["strictCamelCase", "UPPER_CASE"], selector: "variable" },
 				{ format: ["StrictPascalCase"], selector: "typeLike" },
-				{ format: ["UPPER_CASE"], selector: "enumMember" }
+				{ format: ["UPPER_CASE"], selector: "enumMember" },
+				{ selector: ["objectLiteralMethod", "objectLiteralProperty"] }
 			],
 			"@typescript-eslint/no-magic-numbers": "off", // WebGL is basically all magic numbers...
 			"@typescript-eslint/prefer-readonly-parameter-types": "off", // Many μGL functions modify DOM elements and/or WebGL contexts.
